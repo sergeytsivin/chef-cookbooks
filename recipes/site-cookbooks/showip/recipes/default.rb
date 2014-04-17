d61a8e27c44d708550bcb17d3177b40fbd596a72
Sergey Tsivin <stsivin@rbc.ru>
2014-04-17 15:37:14 +0400

    Created showip cookbook

--- recipes/site-cookbooks/showip/recipes/default.rb
+++ recipes/site-cookbooks/showip/recipes/default.rb
@@ -10,10 +10,18 @@
 ##### Print IP of the host only only network
 
 ip = node[:network][:interfaces][:eth1][:addresses].detect{|k,v| v[:family] == "inet" }.first
-log " +=====================================================================================+ "
-log " |                                                                                     | "
-log " | Please add the following line to your /etc/hosts file:                              | "
-log " |                                                                                     | "
-log " | #{ip}   #{node[:showip][:server_name]} "
-log " |                                                                                     | "
-log " +=====================================================================================+ "
+
+log "showip" do
+    message <<EOF
+
++-------------------------------------------------------------------------------------
+|
+| Please add the following line to your /etc/hosts file:
+|
+| #{ip}   #{node[:showip][:server_name]}
+|
++-------------------------------------------------------------------------------------
+
+EOF
+    level :info
+end
